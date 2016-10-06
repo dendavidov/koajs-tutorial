b92738a58d0d3ddbd2c75f621e3dad67a3d4fadf
dendavidov <mail@dendavidov.com>
2016-10-06 22:44:14 +0300

    add example of generator usage

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const koa = require('koa');
 const app = koa();
 
-app.use(function* () {
-  this.body = 'Hello world!';
+app.use(function* (next){
+  console.log('1');
+
+  yield next;
+
+  console.log('2');
+});
+
+app.use(function* (next){
+  console.log('3');
+
+  yield next;
+
+  console.log('4');
+});
+
+app.use(function* (next){
+  console.log('5');
+
+  this.body = "Hello generators!";
+
+  console.log('6');
 });
 
 app.listen(3000, () => {
