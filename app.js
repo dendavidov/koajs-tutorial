e845f0c2326bf85fb3c42042a740508591b2a0a1
dendavidov <mail@dendavidov.com>
2016-10-13 00:53:47 +0300

    error handling

--- app.js
+++ app.js
@@ -7,10 +7,26 @@ function* handle404Errors() {
   this.redirect('/notfound');
 }
 
-//Use the routes defined using the router
-app.use(router.routes());
+function* middlewareWithError(next) {
+  this.throw('Error message', 500);
+}
 
-app.use(handle404Errors);
+//Use the routes defined using the router
+//app.use(router.routes());
+
+//app.use(handle404Errors);
+
+app.use(function* (next) {
+  try {
+    yield next;
+  } catch(err) {
+    this.status = err.status || 500;
+    this.body = err.message;
+    this.app.emit('error', err, this);
+  }
+});
+
+app.use(middlewareWithError);
 
 app.listen(3000, () => {
   console.log('Server running on https://localhost:3000');
