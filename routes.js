d65c6909dfb6ffd706e74c6a9fc91e3a1e824b7e
dendavidov <mail@dendavidov.com>
2016-10-22 01:11:51 +0300

    Cookies

--- routes.js
+++ routes.js
@@ -11,6 +11,14 @@ function* handleForm() {
   this.body = this.request.body
 }
 
+function* setACookie() {
+  this.cookies.set('foo', 'bar', {
+    httpOnly: false,
+    expires: new Date(Date.now() + 60000 * 60 * 24)
+  })
+}
+
+router.get('/', setACookie);
 router.get('/files', renderForm);
 router.post('/upload', handleForm);
 
